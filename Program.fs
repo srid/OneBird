248d2fcb02dae401b8e3f3dd8661bae40e9c0ca1
Sridhar Ratnakumar <srid@srid.ca>
2021-03-21 20:50:24 -0400

    Add memstate types

--- Program.fs
+++ Program.fs
@@ -3,12 +3,38 @@
 open System
 open Memstate
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Tweet =
+    { Id: int
+      Message: string
+    }
+
+type TwitterModel =
+    { Tweets: Tweet list
+    }
+
+// Events 
+
+type Tweeted(tweet: Tweet) =
+    inherit Event()
+
+// Commands
+
+type PostTweet(msg: string) = 
+    inherit Command<TwitterModel, int>()
+
+    override Command.Execute(model: TwitterModel): int =
+        let tweet = { Tweet.Id = 1; Message = msg }
+        Command.RaiseEvent (new Tweeted(tweet))
+        tweet.Id
+
+// Query
+
+type AllTweets() =
+    inherit Query<TwitterModel, Tweet list>()
+
+    override Query.Execute(model: TwitterModel) =
+        model.Tweets
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
     0 // return an integer exit code
